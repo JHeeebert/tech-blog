33b676a5613459fa13979eba3cfe53aac66c2272
Joshua Hebert <jhebertcocktails@gmail.com>
2023-07-25 15:20:34 -0400

    changed file names to json

---
+++ seeds/seed.js
@@ -0,0 +1,19 @@
+const sequelize = require('./config/connection');
+const { User, Post, Comment } = require('./models');
+
+const userData = require('./seeds/userSeeds.json');
+const postData = require('./seeds/postSeeds.json');
+const commentData = require('./seeds/commentSeeds.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+    await Post.bulkCreate(postData);
+    await Comment.bulkCreate(commentData);
+    process.exit(0);
+}
+
+seedDatabase();
