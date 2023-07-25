e7b70db90e9d69a4d7e876489ef7f3b435a733bc
Joshua Hebert <jhebertcocktails@gmail.com>
2023-07-25 05:13:44 -0400

    created editPost.js

---
+++ public/js/editPost.js
@@ -0,0 +1,22 @@
+// async function to edit a post:
+async function editPost(event) {
+    event.preventDefault();
+    const id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#post-content').value.trim();
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title,
+            content
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    }
+    else {
+        alert(response.statusText);
+    }
+}
+document.querySelector('.edit-post-form').addEventListener('submit', editPost);
