9eaa6c078376965499b428eec94790365228304a
Joshua Hebert <jhebertcocktails@gmail.com>
2023-07-26 00:01:09 -0400

    fixed messed up connections

--- config/connection.js
+++ config/connection.js
@@ -14,7 +14,7 @@ if (process.env.JAWSDB_URL) {
         process.env.DB_PW,
         {
             // This is used to connect to the database
-            host: '127.0.0.1',
+            host: 'localhost',
             dialect: 'mysql',
             port: 3306
         }
