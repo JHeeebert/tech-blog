b34584eebdf021dea6628ed7f8056c8ce2ceb42b
Joshua Hebert <jhebertcocktails@gmail.com>
2023-07-31 22:19:54 -0400

    updated seeds files with to fix bugs

--- seeds/postSeeds.js
+++ seeds/postSeeds.js
@@ -2,32 +2,32 @@ const { Post } = require('../models');
 
 const postData = [
    {
-      "Title": "JavaScript Error a life story",
+      "title": "JavaScript Error a life story",
       "post_text": "I get more errors than I get likes on my posts",
       "user_id": 1
    },
    {
-      "Title": "I love Learning JavaScript",
+      "title": "I love Learning JavaScript",
       "post_text": "I love learning JavaScript, it's so much fun!",
       "user_id": 2
    },
    {
-      "Title": "I Hate Learning JavaScript",
+      "title": "I Hate Learning JavaScript",
       "post_text": "I hate learning JavaScript, it makes me cry!",
       "user_id": 3
    },
    {
-      "Title": "I'm a JavaScript Wizard",
+      "title": "I'm a JavaScript Wizard",
       "post_text": "I'm a JavaScript Wizard, I can do anything!",
       "user_id": 4
    },
    {
-      "Title": "I'm a JavaScript Ninja",
+      "title": "I'm a JavaScript Ninja",
       "post_text": "I'm a JavaScript Ninja, I can do anything!",
       "user_id": 5
    },
    {
-      "Title": "I'm a JavaScript Guru",
+      "title": "I'm a JavaScript Guru",
       "post_text": "I'm a JavaScript Guru, I can do anything!",
       "user_id": 6
    }
--- seeds/userSeeds.js
+++ seeds/userSeeds.js
@@ -4,32 +4,38 @@ const userData = [
     {
         "id": 1,
         "name": "John Doe",
-        "email": "Johndoe@example.com"
+        "email": "Johndoe@example.com",
+        "password": "password12345"
     },
     {
         "id": 2,
         "name": "Jane Doe",
-        "email": "Janedoe@example.com"
+        "email": "Janedoe@example.com",
+        "password": "password123456"
     },
     {
         "id": 3,
         "name": "John Smith",
-        "email": "Johnsmith@example.com"
+        "email": "Johnsmith@example.com",
+        "password": "password1234567"
     },
     {
         "id": 4,
         "name": "Jane Smith",
-        "email": "Janesmith@example.com"
+        "email": "Janesmith@example.com",
+        "password": "password12345678"
     },
     {
         "id": 5,
         "name": "Billy Strings",
-        "email": "Billystrings@example.com"
+        "email": "Billystrings@example.com",
+        "password": "password123456789"
     },
     {
         "id": 6,
         "name": "Billy Bob",
-        "email": "Billybob@example.com"
+        "email": "Billybob@example.com",
+        "password": "password1234567890"
     }
 ]
 
