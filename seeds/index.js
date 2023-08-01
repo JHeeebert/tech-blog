34a3e062032169fe8ba84f2c5fcc6f34c1f6ee75
Joshua Hebert <jhebertcocktails@gmail.com>
2023-07-31 22:09:24 -0400

    updated seeds index.js and package.json to reflect

--- seeds/index.js
+++ seeds/index.js
@@ -1,30 +1,20 @@
-const sequelize = require('../config/connection');
-const { User, Post, Comment } = require('../models');
-const userData = require('../seeds/userSeeds.json');
-const postData = require('../seeds/postSeeds.json');
-const commentData = require('../seeds/commentSeeds.json');
+const seedUsers = require('./userSeeds');
+const seedPosts = require('./postSeeds');
+const seedComments = require('./commentSeeds');
 
-const seedDatabase = async () => {
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
     await sequelize.sync({ force: true });
-    await User.bulkCreate(userData, {
-        individualHooks: true,
-        returning: true,
-    });
-    await Post.bulkCreate(postData);
-    await Comment.bulkCreate(commentData);
+    console.log('\n----- DATABASE SYNCED -----\n');
+    await seedUsers();
+    console.log('\n----- USERS SEEDED -----\n');
+    await seedPosts();
+    console.log('\n----- POSTS SEEDED -----\n');
+    await seedComments();
+    console.log('\n----- COMMENTS SEEDED -----\n');
+    
     process.exit(0);
-};
-    seedDatabase().then(() => {
-        process.exit(0);
-    }).catch((err) => {
-        console.log(err);
-        process.exit(1);
-    });
-// Run the following command to seed the database:
-// npm run seed
-// Run the following command to start the application:
-// npm start
-// Run the following command to start the application in development mode:
-// npm run watch
-// Run the following command to run the tests:
-// npm run test
+    }
+    
+seedAll();
